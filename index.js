e0cbd895663248618a20096040be3530b0e04805
Hasan Dokmak <hasandokmak23@gmail.com>
2025-08-15 06:00:42 +0300

    Edits

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inputuser = document.querySelector('.userinput');
 const addtk = document.querySelector('.taskadder');
-
+const tasklist = document.querySelector('.tasklist-display');
 
 addtk.addEventListener('click', function(){
     const ttxt = inputuser.value;
@@ -13,11 +13,14 @@ addtk.addEventListener('click', function(){
     const li = document.createElement("li");
     li.classList.add('eachtask')
     li.innerHTML = ` 
+    <input type="checkbox" class="tomark"/>
     <span class="txtask">${ttxt}</span>
     <button class="edit">Edit</button>
     <button class="delete">Delete</button>
 
 
     `;
-    
+    tasklist.appendChild(li);
+    inputuser.value="";
+
 });
